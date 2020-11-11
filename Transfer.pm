1a7508dd5d506f401c6ea5e1c0c765c0680d0852
Peter Wirdemo <peter.wirdemo@gmail.com>
2020-11-11 20:52:56 +0100

    perltidy

--- Transfer.pm
+++ Transfer.pm
@@ -1,6 +1,5 @@
 package Object;
 
-
 use strict;
 use Carp;
 use vars qw($VERSION);
@@ -8,42 +7,42 @@ use vars qw($VERSION);
 $VERSION = '0.01';
 
 sub set($$$) {
-        my($self) = shift;
-        my($what) = shift;
-        my($value) = shift;
+    my ($self)  = shift;
+    my ($what)  = shift;
+    my ($value) = shift;
 
-        $what =~ tr/a-z/A-Z/;
+    $what =~ tr/a-z/A-Z/;
 
-        $self->{ $what }=$value;
-        return($value);
+    $self->{$what} = $value;
+    return ($value);
 }
 
 sub get($$) {
-        my($self) = shift;
-        my($what) = shift;
+    my ($self) = shift;
+    my ($what) = shift;
 
-        $what =~ tr/a-z/A-Z/;
-        my $value = $self->{ $what };
+    $what =~ tr/a-z/A-Z/;
+    my $value = $self->{$what};
 
-        return($self->{ $what });
+    return ( $self->{$what} );
 }
 
 sub new {
-        my $proto  = shift;
-        my $class  = ref($proto) || $proto;
-        my $self   = {};
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self  = {};
 
-        bless($self,$class);
+    bless( $self, $class );
 
-        my(%args) = @_;
+    my (%args) = @_;
 
-        my($key,$value);
-        while( ($key, $value) = each %args ) {
-                $key =~ tr/a-z/A-Z/;
-                $self->set($key,$value);
-        }
+    my ( $key, $value );
+    while ( ( $key, $value ) = each %args ) {
+        $key =~ tr/a-z/A-Z/;
+        $self->set( $key, $value );
+    }
 
-        return($self);
+    return ($self);
 }
 
 package Transfer;
@@ -55,351 +54,359 @@ use File::Copy;
 use Cwd;
 use Fcntl qw(:flock);
 
-
-my($debug) = 0;
+my ($debug) = 0;
 $Transfer::VERSION = '0.01';
-@Transfer::ISA = qw(Object);
+@Transfer::ISA     = qw(Object);
 
 sub new {
-        my $proto = shift;
-        my $class = ref($proto) || $proto;
-        my $self  = {};
-        bless($self,$class);
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self  = {};
+    bless( $self, $class );
 
-        my(%hash) = ( @_ );
-        while ( my($key,$val) = each(%hash) ) {
-                $self->set($key,$val);
-                if ( $key =~ /debug/i ) {
-                   $debug = $val;
-                }
+    my (%hash) = (@_);
+    while ( my ( $key, $val ) = each(%hash) ) {
+        $self->set( $key, $val );
+        if ( $key =~ /debug/i ) {
+            $debug = $val;
         }
-        my($conf) = $self->get("conf");
-        croak "new needs parameter 'conf'" unless ( defined($conf) );
-        croak "Unable to get lock on $conf" unless ( $self->lock($conf) );
-        croak "Something wrong in $conf" unless ( $self->readconf($conf) );
+    }
+    my ($conf) = $self->get("conf");
+    croak "new needs parameter 'conf'"  unless ( defined($conf) );
+    croak "Unable to get lock on $conf" unless ( $self->lock($conf) );
+    croak "Something wrong in $conf"    unless ( $self->readconf($conf) );
 
-        return($self);
+    return ($self);
 }
 
 sub trim() {
-	my($self) = shift;
-	my($key) = shift;
-	return(undef) unless ( defined($key) );
-	$key =~ s/^\s+//;
-	$key =~ s/\s+$//;
-	return($key);
+    my ($self) = shift;
+    my ($key)  = shift;
+    return (undef) unless ( defined($key) );
+    $key =~ s/^\s+//;
+    $key =~ s/\s+$//;
+    return ($key);
 }
 
 sub readfile() {
-	my($self) = shift;
-	my($file) = shift;
-	my(@res) = ();
-	if ( open(IN,"<$file") ) {
-		foreach ( <IN> ) {
-			chomp;
-			push(@res,$_);
-		}
-		close(IN);
-	}
-	return(@res);
+    my ($self) = shift;
+    my ($file) = shift;
+    my (@res)  = ();
+    if ( open( IN, "<$file" ) ) {
+        foreach (<IN>) {
+            chomp;
+            push( @res, $_ );
+        }
+        close(IN);
+    }
+    return (@res);
 }
 
 sub readconf() {
-  my($self) = shift;
-  my($conf) = shift;
-  my(%res) = ();
+    my ($self) = shift;
+    my ($conf) = shift;
+    my (%res)  = ();
 
-  unless ( defined($conf) ) {
-    print "readconf need \$conf parameter\n";
-    return(undef);
-  }
+    unless ( defined($conf) ) {
+        print "readconf need \$conf parameter\n";
+        return (undef);
+    }
 
-  my(@content) = $self->readfile($conf);
+    my (@content) = $self->readfile($conf);
 
-  foreach ( @content ) {
-    next unless ( defined($_) );
-    chomp;
-    s/#.*//g;
-    s/^\s+//;
-    s/\s+$//;
-    next if ( $_ =~  /^$/ );
-    my($key, $value) = split(/\s*=\s*/,$_);
-    print "key: $key\n" if ( $debug );
-    print "value: $value\n" if ( $debug );
-    next unless ( defined($key) );
-    next unless ( defined($value) );
-    $self->config($key,$value);
-    #$res{$key}=$value;
-  }
+    foreach (@content) {
+        next unless ( defined($_) );
+        chomp;
+        s/#.*//g;
+        s/^\s+//;
+        s/\s+$//;
+        next if ( $_ =~ /^$/ );
+        my ( $key, $value ) = split( /\s*=\s*/, $_ );
+        print "key: $key\n"     if ($debug);
+        print "value: $value\n" if ($debug);
+        next unless ( defined($key) );
+        next unless ( defined($value) );
+        $self->config( $key, $value );
 
-  return($self->validateconf());
+        #$res{$key}=$value;
+    }
 
-   
-  #return(%res);
-       
+    return ( $self->validateconf() );
+
+    #return(%res);
+
 }
 
 sub checkdir() {
-  my($self) = shift;
-  my($dir) = shift;
-  unless ( defined($dir) ) {
-    return(undef);
-  }
-  unless ( $dir =~ /^[\/\w]+$/ ) {
-    print "$dir contains bad characters\n";
-    return(undef);
-  }
-  if ( ! -d $dir ) {
-    print "$dir is not a directory\n";
-    return(undef);
-  }
-  else {
-    return($dir);
-  }
+    my ($self) = shift;
+    my ($dir)  = shift;
+    unless ( defined($dir) ) {
+        return (undef);
+    }
+    unless ( $dir =~ /^[\/\w]+$/ ) {
+        print "$dir contains bad characters\n";
+        return (undef);
+    }
+    if ( !-d $dir ) {
+        print "$dir is not a directory\n";
+        return (undef);
+    }
+    else {
+        return ($dir);
+    }
 }
 
 sub config() {
-  my($self) = shift;
-  my($key) = shift;
-  my($value) = shift;
-  return(undef) unless ( defined($key) );
-  if ( defined($value) ) {
-    return($self->set("config_" . $key,$value));
-  }
-  else {
-    return($self->get("config_" . $key));
-  }
+    my ($self)  = shift;
+    my ($key)   = shift;
+    my ($value) = shift;
+    return (undef) unless ( defined($key) );
+    if ( defined($value) ) {
+        return ( $self->set( "config_" . $key, $value ) );
+    }
+    else {
+        return ( $self->get( "config_" . $key ) );
+    }
 }
 
 sub validateconf() {
-  my($self) = shift;
-  my(%conf) = @_;
+    my ($self) = shift;
+    my (%conf) = @_;
 
-  my($rc);
-  my($src) = $self->config("src");
-  $rc = $self->checkdir($src);
-  unless ( $rc ) {
-    print "config is missing src key\n";
-    return(undef);
-  }
-  my($dst) = $self->config("dst");
-  $rc = $self->checkdir($dst);
-  unless ( $rc ) {
-    print "config is missing dst key\n";
-    return(undef);
-  }
-
-  my($ext) = $self->config("ext");
-  unless ( defined($ext) ) {
-    print "config is missing ext key\n";
-    return(undef);
-  }
-
-  my($sum) = $self->config("sum");
-  unless ( defined($sum) ) {
-    print "config is missing sum key\n";
-    return(undef);
-  }
-  my($bin) = undef;
-  if ( $sum =~ /^\w+$/ ) {
-    if ( -x "/usr/bin/$sum" ) {
-        $bin = "/usr/bin/$sum" 
+    my ($rc);
+    my ($src) = $self->config("src");
+    $rc = $self->checkdir($src);
+    unless ($rc) {
+        print "config is missing src key\n";
+        return (undef);
     }
-    elsif ( -x "/bin/sum" ) {
-      $bin = "/bin/$sum";
+    my ($dst) = $self->config("dst");
+    $rc = $self->checkdir($dst);
+    unless ($rc) {
+        print "config is missing dst key\n";
+        return (undef);
     }
-  }
-       
-  if ( defined($bin) && -x $bin ) {
-    $self->config("sumbin",$bin);
-  }
-  else {
-    print "Missing executable $sum\n";
-    return(undef);
-  }
 
-  my($maxtransfer) = $self->config("maxtransfer");
-  unless ( $maxtransfer ) {
-    $maxtransfer = 2 * 1000 * 1000 * 1000; # 2 GB
-    $self->config("maxtransfer",$maxtransfer);
-  }
+    my ($ext) = $self->config("ext");
+    unless ( defined($ext) ) {
+        print "config is missing ext key\n";
+        return (undef);
+    }
 
-  my($splitbytes) = $self->config("splitbytes");
-  unless ( $splitbytes ) {
-    $splitbytes =  512 * 1000 * 1000; # 512 MB
-    $self->config("splitbytes",$splitbytes);
-  }
+    my ($sum) = $self->config("sum");
+    unless ( defined($sum) ) {
+        print "config is missing sum key\n";
+        return (undef);
+    }
+    my ($bin) = undef;
+    if ( $sum =~ /^\w+$/ ) {
+        if ( -x "/usr/bin/$sum" ) {
+            $bin = "/usr/bin/$sum";
+        }
+        elsif ( -x "/bin/sum" ) {
+            $bin = "/bin/$sum";
+        }
+    }
 
-  return(1);
+    if ( defined($bin) && -x $bin ) {
+        $self->config( "sumbin", $bin );
+    }
+    else {
+        print "Missing executable $sum\n";
+        return (undef);
+    }
+
+    my ($maxtransfer) = $self->config("maxtransfer");
+    unless ($maxtransfer) {
+        $maxtransfer = 2 * 1000 * 1000 * 1000;    # 2 GB
+        $self->config( "maxtransfer", $maxtransfer );
+    }
+
+    my ($splitbytes) = $self->config("splitbytes");
+    unless ($splitbytes) {
+        $splitbytes = 512 * 1000 * 1000;          # 512 MB
+        $self->config( "splitbytes", $splitbytes );
+    }
+
+    return (1);
 }
 
 sub checksum() {
-  my($self) = shift;
-  my($srcext) = shift;
-  my(@cmd) = @_;
-  my(@res) = ();
-  delete @ENV{qw(PATH IFS CDPATH ENV BASH_ENV)};
-  my($sumbin) = $self->config("sumbin");
+    my ($self)   = shift;
+    my ($srcext) = shift;
+    my (@cmd)    = @_;
+    my (@res)    = ();
+    delete @ENV{qw(PATH IFS CDPATH ENV BASH_ENV)};
+    my ($sumbin) = $self->config("sumbin");
 
-  #
-  # Tatinting srcext
-  #
-  unless ($srcext =~ m#^([\w.-]+)$#) {                  # $1 is untainted
-      die "filename '$srcext' has invalid characters.\n";
-  }
-  $srcext = $1; 
-
-  #
-  # Tainting $sumbin
-  #
-  unless ($sumbin =~ m#^([\/\w.-]+)$#) {                  # $1 is untainted
-    die "filename '$sumbin' has invalid characters.\n";
-  }
-  $sumbin = $1;
-
-  open( my $listing, "-|", $sumbin,"--check","--ignore-missing",$srcext) or croak "error executing command: stopped";
-  while (<$listing>) {
-    next unless ( defined($_) );
-    chomp;
-    if ( $_ =~ /:\s+OK/ ) {
-      $_ =~ s/:\s+OK.*//;
-      push(@res,$_);
+    #
+    # Tatinting srcext
+    #
+    unless ( $srcext =~ m#^([\w.-]+)$# ) {    # $1 is untainted
+        die "filename '$srcext' has invalid characters.\n";
     }
-  }
-  close($listing);
-  return(@res);
+    $srcext = $1;
+
+    #
+    # Tainting $sumbin
+    #
+    unless ( $sumbin =~ m#^([\/\w.-]+)$# ) {    # $1 is untainted
+        die "filename '$sumbin' has invalid characters.\n";
+    }
+    $sumbin = $1;
+
+    open( my $listing, "-|", $sumbin, "--check", "--ignore-missing", $srcext )
+      or croak "error executing command: stopped";
+    while (<$listing>) {
+        next unless ( defined($_) );
+        chomp;
+        if ( $_ =~ /:\s+OK/ ) {
+            $_ =~ s/:\s+OK.*//;
+            push( @res, $_ );
+        }
+    }
+    close($listing);
+    return (@res);
 }
 
 sub wait_until_transfered() {
-  my($self) = shift;
-  my($file) = shift;
-  return(undef) unless ( defined($file) );
-  my($dst) = $self->config("dst");
-  my($dstfile) = $dst . "/" . $file;
-  unless ($dstfile =~ m#^([\/\w.-]+)$#) {                  # $1 is untainted
-      die "filename '$dstfile' has invalid characters.\n";
-  }
-  $dstfile = $1;
-  my($start) = time;
-  my($i) = 1;
-  while ( 1 ) {
-    last unless ( -r $dstfile );
-    $i++;
-    print "Wating($i) for $dstfile to be transfered Started:" . localtime($start) . ", Now: " . localtime(time) . "\n";
-    sleep(10);
-  }
-  return($i);
+    my ($self) = shift;
+    my ($file) = shift;
+    return (undef) unless ( defined($file) );
+    my ($dst)     = $self->config("dst");
+    my ($dstfile) = $dst . "/" . $file;
+    unless ( $dstfile =~ m#^([\/\w.-]+)$# ) {    # $1 is untainted
+        die "filename '$dstfile' has invalid characters.\n";
+    }
+    $dstfile = $1;
+    my ($start) = time;
+    my ($i)     = 1;
+    while (1) {
+        last unless ( -r $dstfile );
+        $i++;
+        print "Wating($i) for $dstfile to be transfered Started:"
+          . localtime($start)
+          . ", Now: "
+          . localtime(time) . "\n";
+        sleep(10);
+    }
+    return ($i);
 }
 
 sub mover() {
-  my($self) = shift;
-  my($file) = shift;
-  return(undef) unless ( defined($file) );
+    my ($self) = shift;
+    my ($file) = shift;
+    return (undef) unless ( defined($file) );
 
-  my($src) = $self->config("src");
-  my($dst) = $self->config("dst");
-  my($srcfile) = $src . "/" . $file;
-  my($dstfile) = $dst . "/" . $file;
-  unless ($srcfile =~ m#^([\/\w.-]+)$#) {                  # $1 is untainted
-      die "filename '$srcfile' has invalid characters.\n";
-  }
-  $srcfile = $1;
-  unless ($dstfile =~ m#^([\/\w.-]+)$#) {                  # $1 is untainted
-      die "filename '$dstfile' has invalid characters.\n";
-  }
-  $dstfile = $1;
+    my ($src)     = $self->config("src");
+    my ($dst)     = $self->config("dst");
+    my ($srcfile) = $src . "/" . $file;
+    my ($dstfile) = $dst . "/" . $file;
+    unless ( $srcfile =~ m#^([\/\w.-]+)$# ) {    # $1 is untainted
+        die "filename '$srcfile' has invalid characters.\n";
+    }
+    $srcfile = $1;
+    unless ( $dstfile =~ m#^([\/\w.-]+)$# ) {    # $1 is untainted
+        die "filename '$dstfile' has invalid characters.\n";
+    }
+    $dstfile = $1;
 
-  print "Checking [$_]\n";
-  my($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
-                       $atime,$mtime,$ctime,$blksize,$blocks)
-                           = stat($srcfile);
-  unless ( defined($size) ) {
-    print "unable to stat $_: $!\n";
-    return(undef);
-  }
+    my (
+        $dev,  $ino,   $mode,  $nlink, $uid,     $gid, $rdev,
+        $size, $atime, $mtime, $ctime, $blksize, $blocks
+    ) = stat($srcfile);
+    unless ( defined($size) ) {
+        print "unable to stat $srcfile: $!\n";
+        return (undef);
+    }
 
-  my($rc) = 0;
-  $rc =  move($srcfile, $dstfile);
-  print "move($srcfile,$dstfile) = $rc";
-  unless ( $rc ) {
-    print " (error: $!)";
-  }
-  print "\n";
-  if ( $dstfile =~ /README/ ) {
-    return(0);
-  }
-  return($size);
+    my ($rc) = 0;
+    $rc = move( $srcfile, $dstfile );
+    print "move($srcfile,$dstfile) = $rc";
+    unless ($rc) {
+        print " (error: $!)\n";
+        return(undef);
+    }
+    print "\n";
+    if ( $dstfile =~ /README/ ) {
+        return (0);
+    }
+    return ($size);
 }
 
 sub transfer() {
-  my($self) = shift;
-  my($ext) = $self->config("ext");
-  my($src) = $self->config("src");
-  my($dst) = $self->config("dst");
-  my($sum) = $self->config("sum");
-  my($maxtransfer) = $self->config("maxtransfer");
+    my ($self)        = shift;
+    my ($ext)         = $self->config("ext");
+    my ($src)         = $self->config("src");
+    my ($dst)         = $self->config("dst");
+    my ($sum)         = $self->config("sum");
+    my ($maxtransfer) = $self->config("maxtransfer");
 
-  unless ($src =~ m#^([\/\w.-]+)$#) {                  # $1 is untainted
-      die "filename '$src' has invalid characters.\n";
-  }
-  $src = $1; 
-
-  my $cwd = getcwd;
-  unless ($cwd =~ m#^([\/\w.-]+)$#) {                  # $1 is untainted
-      die "filename '$cwd' has invalid characters.\n";
-  }
-  $cwd = $1;
-  $self->config("cwd",$cwd);
-  chdir($cwd) or die "chdir($cwd): $!\n";
-  chdir($src) or die "chdir($src): $!\n";
-
-  my($srcext);
-  my($totsize) = 0;
-  foreach $srcext ( <*.$ext> ) {
-    print "srcext: $srcext\n";
-    my(@srcext) = $self->checksum($srcext);
-    chdir($cwd) or die "chdir($cwd): $!\n";
-    foreach ( @srcext, $srcext ) {
-      my($size) = 0;
-
-
-
-      my($retries) = 3;
-      while ( $retries-- ) {
-        $size = $self->mover($_);
-        last if ( $size );
-        print "Retrying($retries) transfer of $_, sleeping\n";
-        sleep(5);
-      }
-
-      $totsize += $size;
-      print "totsize: $totsize\n";
-
-      #if ( $totsize > $maxtransfer ) {
-      if ( $totsize > 1000 ) {
-        $self->wait_until_transfered($_);
-        $totsize = 0;
-      }
-
+    unless ( $src =~ m#^([\/\w.-]+)$# ) {    # $1 is untainted
+        die "filename '$src' has invalid characters.\n";
     }
-    #$self->mover($srcext);
+    $src = $1;
+
+    my $cwd = getcwd;
+    unless ( $cwd =~ m#^([\/\w.-]+)$# ) {    # $1 is untainted
+        die "filename '$cwd' has invalid characters.\n";
+    }
+    $cwd = $1;
+    $self->config( "cwd", $cwd );
+    chdir($cwd) or die "chdir($cwd): $!\n";
     chdir($src) or die "chdir($src): $!\n";
-  }
+
+    my ($srcext);
+    my ($totsize) = 0;
+    foreach $srcext (<*.$ext>) {
+        print "srcext: $srcext\n";
+        my (@srcext) = $self->checksum($srcext);
+        chdir($cwd) or die "chdir($cwd): $!\n";
+        my ($srcfile);
+        foreach $srcfile ( @srcext, $srcext ) {
+            my ($size) = 0;
+
+            my ($retries) = 3;
+            while ( $retries-- ) {
+                $size = $self->mover($srcfile);
+                last if ($size);
+                print "Retrying($retries) transfer of $srcfile, sleeping\n";
+                sleep(5);
+            }
+            unless ( defined($size) ) {
+                print "Skipping $srcfile, to many retries\n";
+                next;
+            }
+
+            $totsize += $size;
+            print "totsize: $totsize\n";
+
+            #if ( $totsize > $maxtransfer ) {
+            if ( $totsize > 1000 ) {
+                $self->wait_until_transfered($srcfile);
+                $totsize = 0;
+            }
+
+        }
+
+        #$self->mover($srcext);
+        chdir($src) or die "chdir($src): $!\n";
+    }
 }
 
 sub lock() {
-  my($self) = shift;
-  my($file) = shift;
-  
-  unless ( open(LOCK,"<",$file) ) {
-    print "Reading $file: $!\n";
-    return(undef);
-  }
-  unless ( flock(LOCK,LOCK_EX|LOCK_NB) ) {
-    print "Can't get lock on $file\n";
-    return(undef);
-  }
-  return(1);
+    my ($self) = shift;
+    my ($file) = shift;
+
+    unless ( open( LOCK, "<", $file ) ) {
+        print "Reading $file: $!\n";
+        return (undef);
+    }
+    unless ( flock( LOCK, LOCK_EX | LOCK_NB ) ) {
+        print "Can't get lock on $file\n";
+        return (undef);
+    }
+    return (1);
 }
 
 1;
--- transfer.pl
+++ transfer.pl
@@ -2,26 +2,25 @@
 
 use strict;
 use Data::Dumper;
-my($debug) = 0;
+my ($debug) = 0;
 
 use lib ".";
 use Transfer;
 
-
 use Getopt::Long;
-my $conf   = $0 . ".conf";
+my $conf = $0 . ".conf";
 my $verbose;
 
-GetOptions (
-    "conf=s" => \$conf,
-    "verbose"  => \$verbose
+GetOptions(
+    "conf=s"  => \$conf,
+    "verbose" => \$verbose
 ) or die("Error in command line arguments\n");
 
-print "conf: $conf\n" if ( $debug );
+print "conf: $conf\n" if ($debug);
 
-my($transfer) = new Transfer( debug => $debug, conf => $conf );
-die "Unable to create Transfer object" unless ( $transfer );
+my ($transfer) = new Transfer( debug => $debug, conf => $conf );
+die "Unable to create Transfer object" unless ($transfer);
 
-print Dumper(\$transfer);
+print Dumper( \$transfer );
 
 $transfer->transfer();
